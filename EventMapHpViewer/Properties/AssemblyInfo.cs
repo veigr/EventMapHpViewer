a1da6c365d847ca00cb53bbd5d250354da0c973b
veigr <veigr@cat-ears.net>
2015-11-25 12:46:06 +0900

    バージョン更新

--- EventMapHpViewer/Properties/AssemblyInfo.cs
+++ EventMapHpViewer/Properties/AssemblyInfo.cs
@@ -43,4 +43,4 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.5.2.0")]
